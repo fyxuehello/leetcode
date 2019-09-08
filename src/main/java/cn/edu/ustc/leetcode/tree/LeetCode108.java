845a6d15972c22e475a75abd68f6b679f55a7dc9
fyxue <fyxue@mail.ustc.edu.cn>
2019-09-08 13:39:50 +0800

    leetcode108

---
+++ src/main/java/cn/edu/ustc/leetcode/tree/LeetCode108.java
@@ -0,0 +1,45 @@
+package cn.edu.ustc.leetcode.tree;
+/*
+题目：
+给定一个排序的数组，将其转变为二叉查找树；
+Given the sorted array: [-10,-3,0,5,9],
+
+One possible answer is: [0,-3,9,-10,null,5], which represents the following height balanced
+思路：
+二叉查找树（Binary Search Tree），（又：二叉搜索树，二叉排序树）它或者是一棵空树，
+或者是具有下列性质的二叉树：
+若它的左子树不空，则左子树上所有结点的值均小于它的根结点的值；
+若它的右子树不空，则右子树上所有结点的值均大于它的根结点的值； 它的左、右子树也分别为二叉排序树。
+ */
+
+import cn.edu.ustc.leetcode.util.TreeNode;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class LeetCode108 {
+    public TreeNode sortedArrayToBST(int[] nums) {
+        return helper(nums,0,nums.length);
+    }
+    private TreeNode helper(int[] nums,int left,int right){
+        if(left>right)
+            return null;
+        int mid=left+(right-left)/2;
+        TreeNode root=new TreeNode(nums[mid]);
+        root.left=helper(nums,left,mid-1);
+        root.right=helper(nums,mid+1,right);
+
+        return root;
+    }
+}
