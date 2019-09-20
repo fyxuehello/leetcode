700c7a7c6ffee258c7bf5b0f4a354b3df3631222
fyxue <fyxue@mail.ustc.edu.cn>
2019-09-20 12:40:06 +0800

    leetcode5

---
+++ src/main/java/cn/edu/ustc/leetcode/dynamicProgramming/LeetCode5.java
@@ -0,0 +1,21 @@
+package cn.edu.ustc.leetcode.dynamicProgramming;
+
+public class LeetCode5 {
+    public String longestPalindrome(String s) {
+        if(s==null||s.length()<2)
+            return s;
+
+        String res=null;
+        int n=s.length();
+        boolean[][] dp=new boolean[n][n];
+        for(int i=n-1;i>=0;i--){
+            for(int j=i;j<n;j++){
+                dp[i][j]=s.charAt(i)==s.charAt(j)&&(j-i<3||dp[i+1][j-1]);
+
+                if(dp[i][j]&&(res==null||j-i+1>res.length()))
+                    res=s.substring(i,j+1);
+            }
+        }
+        return res;
+    }
+}
