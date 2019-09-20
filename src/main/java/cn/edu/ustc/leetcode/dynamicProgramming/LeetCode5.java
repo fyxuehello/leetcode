3cd8dc994a5430d69f4149462d18a3fbc08176ab
fyxue <fyxue@mail.ustc.edu.cn>
2019-09-20 13:08:41 +0800

    leetcode5

--- src/main/java/cn/edu/ustc/leetcode/dynamicProgramming/LeetCode5.java
+++ src/main/java/cn/edu/ustc/leetcode/dynamicProgramming/LeetCode5.java
@@ -1,5 +1,13 @@
 package cn.edu.ustc.leetcode.dynamicProgramming;
+/*
+题目：
+给定一个字符串，返回其最长回文子串；
+思路：
+使用动态规划解题；当我们想到用动态规划解题时，第一个问题就是它的子问题是什么？如何把原问题和子问题连接起来；
 
+我们使用一个二维数组dp来表示(i,j)是否回文串，如果是的话就为true，否则为false；
+
+ */
 public class LeetCode5 {
     public String longestPalindrome(String s) {
         if(s==null||s.length()<2)
