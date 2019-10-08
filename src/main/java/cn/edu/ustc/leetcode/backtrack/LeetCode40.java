657c53ae541532f2373373a21ff1054dbb823bdd
fyxue <fyxue@mail.ustc.edu.cn>
2019-10-08 13:05:50 +0800

    leetcode40

--- src/main/java/cn/edu/ustc/leetcode/backtrack/LeetCode40.java
+++ src/main/java/cn/edu/ustc/leetcode/backtrack/LeetCode40.java
@@ -5,9 +5,9 @@ import java.util.List;
 
 /*
 题目：
-Given a set of candidate numbers (candidates) (without duplicates) and a target number (target), find all unique combinations in candidates where the candidate numbers sums to target.
+Given a collection of candidate numbers (candidates) and a target number (target), find all unique combinations in candidates where the candidate numbers sums to target.
 
-The same repeated number may be chosen from candidates unlimited number of times.
+Each number in candidates may only be used once in the combination.
 
 Note:
 
@@ -15,26 +15,27 @@ All numbers (including target) will be positive integers.
 The solution set must not contain duplicate combinations.
 Example 1:
 
-Input: candidates = [2,3,6,7], target = 7,
+Input: candidates = [10,1,2,7,6,1,5], target = 8,
 A solution set is:
 [
-  [7],
-  [2,2,3]
+  [1, 7],
+  [1, 2, 5],
+  [2, 6],
+  [1, 1, 6]
 ]
 Example 2:
 
-Input: candidates = [2,3,5], target = 8,
+Input: candidates = [2,5,2,1,2], target = 5,
 A solution set is:
 [
-  [2,2,2,2],
-  [2,3,3],
-  [3,5]
+  [1,2,2],
+  [5]
 ]
 思路：
 
 */
-public class LeetCode39 {
-    public List<List<Integer>> combinationSum(int[] candidates, int target) {
+public class LeetCode40 {
+    public List<List<Integer>> combinationSum2(int[] candidates, int target) {
         List<List<Integer>> res = new ArrayList<>();
         backtrack(candidates, res, new ArrayList<Integer>(), target, 0);
         return res;
@@ -47,8 +48,10 @@ public class LeetCode39 {
             res.add(new ArrayList<Integer>(list));
         else {
             for (int i = start; i < candidates.length; i++) {
+                if(i>start&&candidates[i]==candidates[i-1])
+                    continue;
                 list.add(candidates[i]);
-                backtrack(candidates, res, list, remain - candidates[i], i);
+                backtrack(candidates, res, list, remain - candidates[i], i + 1);
                 list.remove(list.size() - 1);
             }
         }
