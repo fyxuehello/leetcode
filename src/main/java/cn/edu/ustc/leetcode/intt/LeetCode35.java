c5488d49f9bdd7620acd07528631a2b2e189fb3c
fyxue <fyxue@mail.ustc.edu.cn>
2019-10-10 13:40:47 +0800

    leetcode35

--- src/main/java/cn/edu/ustc/leetcode/intt/LeetCode35.java
+++ src/main/java/cn/edu/ustc/leetcode/intt/LeetCode35.java
@@ -28,18 +28,18 @@ Output: 0
 public class LeetCode35 {
     //二分查找，找到第一个等于或大于target的位置
     public int searchInsert(int[] nums, int target) {
-        if(nums.length==0)
+        if (nums.length == 0)
             return 0;
-        int low=0,high=nums.length-1;
-        while(low<=high){
-            int mid=low+(high-low)/2;
-            if(nums[mid]<target)
-                low=mid+1;
-            else if(nums[mid]>target)
-                high=mid-1;
+        int low = 0, high = nums.length - 1;
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            if (nums[mid] < target)
+                low = mid + 1;
+            else if (nums[mid] > target)
+                high = mid - 1;
             else
                 return mid;
-            }
+        }
         return low;
     }
 }
