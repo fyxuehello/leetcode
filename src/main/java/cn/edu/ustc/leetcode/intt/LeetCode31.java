e0d14746afa3484a94cc61f7cc47bb52dce02130
fyxue <fyxue@mail.ustc.edu.cn>
2019-10-10 14:08:04 +0800

    leetcode31

--- src/main/java/cn/edu/ustc/leetcode/intt/LeetCode31.java
+++ src/main/java/cn/edu/ustc/leetcode/intt/LeetCode31.java
@@ -19,40 +19,42 @@ Here are some examples. Inputs are in the left-hand column and its corresponding
 */
 public class LeetCode31 {
     public void nextPermutation(int[] nums) {
-        if(nums.length<2)
+        if (nums.length < 2)
             return;
-        int n=nums.length,pos=-1,posValue=0;
+        int n = nums.length, pos = -1, posValue = 0;
         //从后向前找到第一个不递增的数字
-        for(int i=n-1;i>=0;i--){
-            if(nums[i]<nums[i+1]){
-                pos=i;
-                posValue=nums[i];
+        for (int i = n - 1; i >= 0; i--) {
+            if (nums[i] < nums[i + 1]) {
+                pos = i;
+                posValue = nums[i];
                 break;
             }
         }
         //6,5,4,3,2,1的情况
-        if(pos==-1){
-            reverse(nums,0,n);
+        if (pos == -1) {
+            reverse(nums, 0, n);
             return;
         }
         //从后向前找到第一个大于pos位置上的数字，并交换
-        for(int i=n;i>=0;i--){
-            if(nums[i]>posValue){
-                swap(nums,i,pos);
+        for (int i = n; i >= 0; i--) {
+            if (nums[i] > posValue) {
+                swap(nums, i, pos);
                 break;
             }
         }
         //从pos+1的位置开始往后反转
-        reverse(nums,pos+1,n);
+        reverse(nums, pos + 1, n);
     }
-    private void swap(int[] nums,int i,int j){
-        int temp=nums[i];
-        nums[i]=nums[j];
-        nums[j]=temp;
+
+    private void swap(int[] nums, int i, int j) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
     }
-    private void reverse(int[] nums,int i,int j){
-        while(i<j){
-            swap(nums,i,j);
+
+    private void reverse(int[] nums, int i, int j) {
+        while (i < j) {
+            swap(nums, i, j);
             i++;
             j--;
         }
