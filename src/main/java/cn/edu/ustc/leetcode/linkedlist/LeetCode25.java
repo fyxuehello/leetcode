1e99a445c6a5cb03019b76e34a0156c435f33cc5
fyxue <fyxue@mail.ustc.edu.cn>
2019-10-11 18:17:32 +0800

    leetcode25

---
+++ src/main/java/cn/edu/ustc/leetcode/linkedlist/LeetCode25.java
@@ -0,0 +1,57 @@
+package cn.edu.ustc.leetcode.linkedlist;
+
+import cn.edu.ustc.leetcode.util.ListNode;
+
+import java.util.List;
+/*
+题目：
+Given a linked list, reverse the nodes of a linked list k at a time and return its modified list.
+
+k is a positive integer and is less than or equal to the length of the linked list. If the number of nodes is not a multiple of k then left-out nodes in the end should remain as it is.
+
+Example:
+
+Given this linked list: 1->2->3->4->5
+
+For k = 2, you should return: 2->1->4->3->5
+
+For k = 3, you should return: 3->2->1->4->5
+
+Note:
+
+Only constant extra memory is allowed.
+You may not alter the values in the list's nodes, only nodes itself may be changed.
+思路：
+首先，找到第k+1个节点，然后将前k个节点反转。k+1节点之后递归调用。
+ */
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ * int val;
+ * ListNode next;
+ * ListNode(int x) { val = x; }
+ * }
+ */
+public class LeetCode25 {
+    public ListNode reverseKGroup(ListNode head, int k) {
+        ListNode cur = head;
+        int count = 0;
+        while (cur != null && count != k) {
+            cur=cur.next;
+            count++;
+        }
+        if(count==k){//找到第k 个节点
+            cur=reverseKGroup(cur,k);
+
+            while(count-->0){
+                ListNode temp=head.next;
+                head.next=cur;
+                cur=head;
+                head=temp;
+            }
+            head=cur;
+        }
+        return head;
+    }
+}
