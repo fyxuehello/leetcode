bfe537c9e5612914a4b6837992fc227d57e3f498
fyxue <fyxue@mail.ustc.edu.cn>
2019-10-21 20:09:13 +0800

    LeetCode 138

--- src/main/java/cn/edu/ustc/leetcode/linkedlist/LeetCode138.java
+++ src/main/java/cn/edu/ustc/leetcode/linkedlist/LeetCode138.java
@@ -23,49 +23,49 @@ Node 2's value is 2, its next pointer points to null and its random pointer poin
 /**
  * // Definition for a Node.
  * class Node {
- *     public int val;
- *     public Node next;
- *     public Node random;
- *
- *     public Node() {}
- *
- *     public Node(int _val,Node _next,Node _random) {
- *         val = _val;
- *         next = _next;
- *         random = _random;
- *     }
+ * public int val;
+ * public Node next;
+ * public Node random;
+ * <p>
+ * public Node() {}
+ * <p>
+ * public Node(int _val,Node _next,Node _random) {
+ * val = _val;
+ * next = _next;
+ * random = _random;
+ * }
  * };
  */
 public class LeetCode138 {
     public Node copyRandomList(Node head) {
-        if(head==null)
+        if (head == null)
             return null;
         //第一轮循环，将复制的节点添加到原来的链表当中
-        Node cur=head;
-        while(cur!=null){
-            Node next=cur.next;
-            cur.next=new Node(cur.val,next,null);
-            cur=next;
+        Node cur = head;
+        while (cur != null) {
+            Node next = cur.next;
+            cur.next = new Node(cur.val, next, null);
+            cur = next;
         }
         //第二轮循环是把复制节点的随机指针加上
-        cur=head;
-        while(cur!=null){
-            if(cur.random!=null){
-                cur.next.random=cur.random.next;
+        cur = head;
+        while (cur != null) {
+            if (cur.random != null) {
+                cur.next.random = cur.random.next;
             }
-            cur=cur.next.next;
+            cur = cur.next.next;
         }
         //
-        cur=head;
-        Node copyHead=cur.next;
-        while(cur!=null){
-            Node next=cur.next.next;
-            Node copy=cur.next;
-            cur.next=next;
-            if(next!=null){
-                copy.next=next.next;
+        cur = head;
+        Node copyHead = cur.next;
+        while (cur != null) {
+            Node next = cur.next.next;
+            Node copy = cur.next;
+            cur.next = next;
+            if (next != null) {
+                copy.next = next.next;
             }
-            cur=next;
+            cur = next;
         }
         return copyHead;
     }
