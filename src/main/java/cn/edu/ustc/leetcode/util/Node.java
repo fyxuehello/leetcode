5d312906652a0a6a7640c9df15b649d35500c6e4
fyxue <fyxue@mail.ustc.edu.cn>
2019-10-21 19:59:51 +0800

    LeetCode 138

--- src/main/java/cn/edu/ustc/leetcode/util/Node.java
+++ src/main/java/cn/edu/ustc/leetcode/util/Node.java
@@ -15,4 +15,13 @@ public class Node {
         right = _right;
         next = _next;
     }
+    public static Node random;
+
+
+    public Node(int _val,Node _next,Node _random) {
+        val = _val;
+        next = _next;
+        random = _random;
+    }
+
 }
