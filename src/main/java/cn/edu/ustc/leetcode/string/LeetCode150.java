22b74caba6837b3506fa3f2695159bb215071464
fyxue <fyxue@mail.ustc.edu.cn>
2019-10-22 13:15:42 +0800

    LeetCode 150

---
+++ src/main/java/cn/edu/ustc/leetcode/string/LeetCode150.java
@@ -0,0 +1,63 @@
+package cn.edu.ustc.leetcode.string;
+
+import java.util.Stack;
+
+/*
+题目：
+Evaluate the value of an arithmetic expression in Reverse Polish Notation.
+
+Valid operators are +, -, *, /. Each operand may be an integer or another expression.
+
+Note:
+
+Division between two integers should truncate toward zero.
+The given RPN expression is always valid. That means the expression would always evaluate to a result and there won't be any divide by zero operation.
+Example 1:
+
+Input: ["2", "1", "+", "3", "*"]
+Output: 9
+Explanation: ((2 + 1) * 3) = 9
+Example 2:
+
+Input: ["4", "13", "5", "/", "+"]
+Output: 6
+Explanation: (4 + (13 / 5)) = 6
+Example 3:
+
+Input: ["10", "6", "9", "3", "+", "-11", "*", "/", "*", "17", "+", "5", "+"]
+Output: 22
+Explanation:
+  ((10 * (6 / ((9 + 3) * -11))) + 17) + 5
+= ((10 * (6 / (12 * -11))) + 17) + 5
+= ((10 * (6 / -132)) + 17) + 5
+= ((10 * 0) + 17) + 5
+= (0 + 17) + 5
+= 17 + 5
+= 22
+思路：
+
+ */
+public class LeetCode150 {
+    public int evalRPN(String[] tokens) {
+        int a,b;
+        Stack<Integer> stack=new Stack<>();
+        for(String s:tokens){
+            if(s.equals("+")){
+                stack.push(stack.pop()+stack.pop());
+            }else if(s.equals("-")){
+                b=stack.pop();
+                a=stack.pop();
+                stack.push(a-b);
+            }else if(s.equals("*")){
+                stack.push(stack.pop()*stack.pop());
+            }else if(s.equals("/")){
+                b=stack.pop();
+                a=stack.pop();
+                stack.push(a/b);
+            }else{
+                stack.push(Integer.parseInt(s));
+            }
+        }
+        return stack.pop();
+    }
+}
