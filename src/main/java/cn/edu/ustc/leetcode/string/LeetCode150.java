85aba5e9a1c63a0307938328536ba6d202a66dd8
fyxue <fyxue@mail.ustc.edu.cn>
2019-10-22 14:02:25 +0800

    LeetCode 150

--- src/main/java/cn/edu/ustc/leetcode/string/LeetCode150.java
+++ src/main/java/cn/edu/ustc/leetcode/string/LeetCode150.java
@@ -39,22 +39,22 @@ Explanation:
  */
 public class LeetCode150 {
     public int evalRPN(String[] tokens) {
-        int a,b;
-        Stack<Integer> stack=new Stack<>();
-        for(String s:tokens){
-            if(s.equals("+")){
-                stack.push(stack.pop()+stack.pop());
-            }else if(s.equals("-")){
-                b=stack.pop();
-                a=stack.pop();
-                stack.push(a-b);
-            }else if(s.equals("*")){
-                stack.push(stack.pop()*stack.pop());
-            }else if(s.equals("/")){
-                b=stack.pop();
-                a=stack.pop();
-                stack.push(a/b);
-            }else{
+        int a, b;
+        Stack<Integer> stack = new Stack<>();
+        for (String s : tokens) {
+            if (s.equals("+")) {
+                stack.push(stack.pop() + stack.pop());
+            } else if (s.equals("-")) {
+                b = stack.pop();
+                a = stack.pop();
+                stack.push(a - b);
+            } else if (s.equals("*")) {
+                stack.push(stack.pop() * stack.pop());
+            } else if (s.equals("/")) {
+                b = stack.pop();
+                a = stack.pop();
+                stack.push(a / b);
+            } else {
                 stack.push(Integer.parseInt(s));
             }
         }
