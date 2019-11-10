e602eea220291d1ca3cce7098d9bcd8a7ea00f1e
fyxue <fyxue@mail.ustc.edu.cn>
2019-11-10 14:46:15 +0800

    jianzhioffer

--- src/main/java/cn/edu/ustc/leetcode/tree/LeetCode103.java
+++ src/main/java/cn/edu/ustc/leetcode/tree/LeetCode103.java
@@ -27,33 +27,34 @@ import java.util.Queue;
 /**
  * Definition for a binary tree node.
  * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode(int x) { val = x; }
+ * int val;
+ * TreeNode left;
+ * TreeNode right;
+ * TreeNode(int x) { val = x; }
  * }
  */
 public class LeetCode103 {
     public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
-        List<List<Integer>> res=new ArrayList<>();
-        Queue<TreeNode> queue=new LinkedList<>();
+        List<List<Integer>> res = new ArrayList<>();
+        Queue<TreeNode> queue = new LinkedList<>();
 
-        if(root==null)
+        if (root == null)
             return res;
         queue.add(root);
-        boolean zigzag=false;while(!queue.isEmpty()){
-            List<Integer> subList=new ArrayList<>();
-            int levelNum=queue.size();
-            for(int i=0;i<levelNum;i++){
-                if(queue.peek().left!=null)
+        boolean zigzag = false;
+        while (!queue.isEmpty()) {
+            List<Integer> subList = new ArrayList<>();
+            int levelNum = queue.size();
+            for (int i = 0; i < levelNum; i++) {
+                if (queue.peek().left != null)
                     queue.add(queue.peek().left);
-                if(queue.peek().right!=null){
+                if (queue.peek().right != null) {
                     queue.add(queue.peek().right);
                 }
-                if(zigzag==false){
+                if (zigzag == false) {
                     subList.add(queue.poll().val);
-                }else{
-                    subList.add(0,queue.poll().val);
+                } else {
+                    subList.add(0, queue.poll().val);
                 }
             }
             res.add(subList);
